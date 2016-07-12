73511adc6a6086815d090a7b4031b38481c897f6
sultana <sultanaisgneiss@gmail.com>
2016-07-12 01:24:31 +0000

    assignment 1 file

---
+++ original.py
@@ -0,0 +1,3 @@
+# For the integers 1 through 50, 
+# find the sum of even numbers
+# MINUS the sum of numbers that are divisible by 7.  
---
+++ refactored.py
@@ -0,0 +1,19 @@
+# For the integers 1 through 50, find the sum of even numbers
+# MINUS the sum of numbers that are divisible by 7
+
+'''* get the first fifty even numbers
+* what is the sum of the even numbers?
+* what even numbers are divisible by 7'''
+
+def even(number):
+    '''Get even numbers'''
+    return (number % 2) == 0
+
+def get_first_50(numberi):
+    '''Get a list of the first fifty even numbers'''
+    return list(range(1, 51))
+
+def even_numbers(list_numbers):
+
+ 
+    
